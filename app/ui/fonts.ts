cac1eda7dad9fef3cb43b78330f54461b34ee3df
Igor Kovryzhkin <igorkovr@gmail.com>
2024-01-16 13:07:41 +0100

    Add lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,12 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+  })
+
+  export const lusitana = Lusitana({
+    weight: "400",
+    subsets: ['latin'],
+    display: 'swap',
+  })
