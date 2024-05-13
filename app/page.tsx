abc91e72bbb40882d527d175355f9a3048c2945f
Igor Kovryzhkin <igorkovr@gmail.com>
2024-05-13 17:08:18 +0200

    Rename to Bacon

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Darb.</strong> Darb is Stripe for transportation.{' '}
-            <a href="https://www.linkedin.com/in/abulmohsenalbabtain" className="text-blue-500">
-              Chech it out
+            <strong>Welcome to Bacon.</strong> Bacon is a secure, enterprise AI platform specifically designed for top-tier financial institutions.{' '}
+            <a href="https://www.bacon.tech" className="text-blue-500">
+              Check it out
             </a>
             <br/>
-            Brought to you by IK.
+            Brought to you by Vegan Speck GmbH.
           </p>
           <Link
             href="/login"
