1ccffe2421965ac64f0e84dac261a630cdd8386b
Igor Kovryzhkin <igorkovr@gmail.com>
2024-05-14 16:22:21 +0200

    fix chat page embedding

--- app/dashboard/chat/page.tsx
+++ app/dashboard/chat/page.tsx
@@ -33,19 +33,18 @@ const Home = () => {
     }, [setMessages, completion, isLoading])
   
     return (
-      <div className="z-10 flex h-screen flex-col gap-5 p-0">
+      <div className="z-10 flex h-full flex-col gap-5 p-0">
         <header className="flex items-center justify-between border-b px-6 py-3">
           {/* "flex" class sets the display property to flex for the header element.
               "items-center" class aligns the items in the header along the center of the cross-axis.
               "justify-between" class justifies the content in the header with space between the items.
               "border-b" class adds a bottom border to the header element.*/}
           <h1 className="text-xl font-bold">AI Chat</h1>
-          <div className="flex items-center gap-3">
-          </div>
+          
         </header>
+        <div className="flex-grow flex-col items-center gap-3">
         <Chat messages={messages} />
-        {/* // TODO:  Add Separator */}
-        {/* <Separator /> */}
+        </div>
         <Chat.Input
           onChange={handleInputChange}
           value={input}
@@ -55,7 +54,7 @@ const Home = () => {
         <div
           className="flex cursor-pointer items-center gap-2 text-xs text-red-500"
           onClick={clearMessages}>
-          <TrashIcon className="h-4 w-4" /> Clear Chat
+          <TrashIcon className="h-4 w-4" />Clear Chat
         </div>
       </div>
     )
