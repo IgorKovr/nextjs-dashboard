d6e05728cf69c6795d4fde3c58d17ce330048c04
Igor Kovryzhkin <igorkovr@gmail.com>
2024-06-19 18:00:58 +0200

    Update UI to upload Files

---
+++ app/ui/chat/FileUpload/FileItem.tsx
@@ -0,0 +1,18 @@
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { cn } from '@/app/lib/utils'
+
+type Props = {
+  name: string
+  onRemove: () => void
+  className?: string
+}
+const FileItem = ({name, className, onRemove}: Props) => {
+  return (
+    <div className={cn('flex items-center', className)}>
+      <label>{name}</label> 
+      {/* <XMarkIcon className="h-4 w-4" onClick={onRemove} /> */}
+    </div>
+  )
+}
+
+export default FileItem
