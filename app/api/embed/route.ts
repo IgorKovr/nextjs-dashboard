17e498a083098aa3d8943205bdc04a071bc1df83
Igor Kovryzhkin <igorkovr@gmail.com>
2024-06-26 15:07:45 +0200

    Update File Splitter, PDF File Loader, prompt, Weaviate upload and retrieval

--- app/api/embed/route.ts
+++ app/api/embed/route.ts
@@ -1,10 +1,25 @@
 import { NextResponse } from 'next/server'
 import weaviate, { vectorizer, generative, WeaviateClient } from 'weaviate-client'
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+// Embedding model
+// More information on available openAI Embedding models:
+// https://openai.com/index/new-embedding-models-and-api-updates/
+const OPENAI_EMBEDDING_MODEL = `text-embedding-3-small`
+// const OPENAI_EMBEDDING_MODEL = `text-embedding-3-large`
+
+// Available dimensions:
+// text-embedding-3-large: 256, 1024, 3072 (default)
+// text-embedding-3-small: 512, 1536 (default)
+const VECTOR_DB_DIMENTIONS = 1536
 
 // Function to embed the data to a Vactor DB
 export async function POST(request: Request) {
   const data = request.formData()
   
+  // Weaviate
+  // TODO: Move to a separate class with an abstraction layer
   const waviateAPIKey = process.env.WEAVIATE_API_KEY ?? ''
   if (!waviateAPIKey) {
     return NextResponse.json({ message: 'Missing Weaviate API Key ENV Variable', success: false });
@@ -25,9 +40,6 @@ export async function POST(request: Request) {
     return NextResponse.json({message: 'Missing file input', success: false})
   }
 
-  const fileContent = await file.text()
-  const fileTitle = await file.name
-
   const weaviateClient: WeaviateClient = await weaviate.connectToWeaviateCloud(
     weaviateEndpointURL, {
       authCredentials: new weaviate.ApiKey(waviateAPIKey),
@@ -37,10 +49,64 @@ export async function POST(request: Request) {
     }
   )
 
+  async function createStorage() {
+    await weaviateClient.collections.create({
+      name: 'Files',
+      properties: [
+        {
+          name: 'title',
+          dataType: 'text' as const,
+        },
+      ],
+      vectorizers: [
+        weaviate.configure.vectorizer.text2VecOpenAI({
+          name: 'title_vector',
+          sourceProperties: ['title'],
+          model: OPENAI_EMBEDDING_MODEL,
+          dimensions: VECTOR_DB_DIMENTIONS
+          },
+        ),
+      ],
+    });
+  }
+
+  // TODO: Add a check if the Storage exist and recreate one when needed
+  // createStorage()
+
+  // File Loaders
+  // PDF Loader 
+  // TODO: Add Multiple File loader (to parse any filetype)
+  const loader = new PDFLoader(file, { splitPages: false })
+  const docs = await loader.load() 
+
+  const fileContent = docs[0].pageContent
+  const fileTitle = docs[0].metadata.title
+
+  console.log(docs[0].pageContent)
+  console.log(docs[0].metadata)
+
+  // Text Splitter
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+    separators:['\n']
+  })
+
+  // TODO: Populate chunks with metadata (context, title, summarization) 
+  const splitDocs = await textSplitter.createDocuments([fileContent])
+  const mappedDocs = splitDocs.map((doc, index) => ({
+    title: fileTitle,
+    content: doc.pageContent,
+    chunk: `chunk ${index + 1}`,
+    metadata: doc.metadata
+  }))
+
+  console.log(fileContent)  
+
   async function uploadDataToWeaviate() {
     const filesCollection = weaviateClient.collections.get('Files');
-    const response = await filesCollection.data.insert({title: fileTitle, content: fileContent})
-    console.log('File uploaded to Weaviate DB:', file?.name, response);
+    const response = await filesCollection.data.insertMany(mappedDocs)
+    console.log('File chunked and uploaded to Weaviate Vector DB:', file?.name, response);
   }
 
   await uploadDataToWeaviate();
